0b482cb25e06e2e872629b67eb9557972165a165
jun <ggprgrkjh@naver.com>
2023-12-08 16:50:04 +0900

    feat(outputview): 할인 전 총주문 금액 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -17,5 +17,10 @@ public class OutputView {
         System.out.println(reservation.getMenusAndCount());
     }
 
+    public static void printTotalOrderAmount(int totalOrderAmount) {
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(totalOrderAmount);
+    }
+
 
 }
