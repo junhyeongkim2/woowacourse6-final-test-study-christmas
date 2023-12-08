b81f1839e7c5e0b7cfd5568734456f5a16ba9238
jun <ggprgrkjh@naver.com>
2023-12-08 16:38:42 +0900

    feat(outputview): 플래너 시작 메세지 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,9 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class OutputView {
+    public static void printPlannerStartMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
 }
